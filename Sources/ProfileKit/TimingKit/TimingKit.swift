c298abb81d4ce62053efc9cfefa47c2970c3b370
DenisSolodovnik <garbboxg@gmail.com>
2026-02-20 21:44:06 +0700

    Add TimingKitParameters, add documentation

--- Sources/ProfileKit/TimingKit/TimingKit.swift
+++ Sources/ProfileKit/TimingKit/TimingKit.swift
@@ -7,33 +7,68 @@
 
 import Foundation
 
+public struct TimingKitParameters {
+
+    let logEnabled: Bool
+    let prefix: String
+    let decimalCount: Int
+    let skipFirstChecks: Int
+    let numberOfChecks: Int
+
+    /// Options Constructor
+    /// - Parameters:
+    ///   - logEnabled: Should the result be printed to the console?
+    ///   - decimalCount: Number of decimal part
+    ///   - numberOfChecks: Number of code checks
+    public init(
+        logEnabled: Bool = true,
+        prefix: String = "Test result",
+        decimalCount: Int = 5,
+        skipFirstChecks: Int = 50,
+        numberOfChecks: Int = 150
+    ) {
+        self.logEnabled = logEnabled
+        self.prefix = prefix
+        self.decimalCount = decimalCount
+        self.skipFirstChecks = skipFirstChecks
+        self.numberOfChecks = numberOfChecks
+    }
+}
+
 #if PROFILERKIT_ENABLED
+
+// TimingKit is used to test code execution speed.
 
 public struct TimingKit {
 
-    public init() {}
-
-    @discardableResult
+    /// Testing code execution speed
+    /// - Parameters:
+    ///   - function: your code closure
+    ///   - parameters: Fine tuning parameters
+    /// - Returns: Code execution time in seconds
     public func test(
         function: () -> Void,
-        count: Int = 15,
-        name: String = #function
+        parameters: TimingKitParameters = .init()
     ) -> Double {
 
-        let maxCount = max(count, 15)
+        let count = max(parameters.numberOfChecks, 15)
 
         var timings: [Double] = []
-        for index in 0..<maxCount {
+        for index in 0..<count {
             let startTime = CFAbsoluteTimeGetCurrent()
             function()
             let endTime = CFAbsoluteTimeGetCurrent() - startTime
-            if index > 3 {
+            if index > parameters.skipFirstChecks {
                 timings.append(endTime)
             }
         }
 
         let timing = timings.reduce(0, +) / Double(count)
-        print(String(format: "%s: %.5f sec.", name, timing))
+
+        if parameters.logEnabled {
+            let str = String(format: "\(parameters.prefix): %.\(parameters.decimalCount)f sec.", timing)
+            print(str)
+        }
 
         return timing
     }
@@ -43,10 +78,11 @@ public struct TimingKit {
 public struct TimingKit {
 
     public init() {}
+
+    @discardableResult
     public func test(
         function: () -> Void,
-        count: Int = 15,
-        name: String = #function
+        parameters: TimingKitParameters = .init()
     ) -> Double {
         return -1
     }
